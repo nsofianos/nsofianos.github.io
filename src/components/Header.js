735e23b06b712fdcf757dddc6e8edb7e4cecc397
nsofianos <nsofianos@hotmail.com>
2022-03-31 13:39:56 -0700

    create header component

---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+function Header() {
+  return (
+    <header className="App-header">
+      <div class="logo">
+        <img src="images/NS.png" alt=""></img>
+      </div>
+      <button class="nav-toggle">
+        <span class="hamburger"></span>
+      </button>
+      <nav class="nav">
+        <ul class="nav__list">
+          <li class="nav__item">
+            <a href="#" class="nav__link">
+              Home
+            </a>
+          </li>
+          <li class="nav__item">
+            <a href="#" class="nav__link">
+              My Services
+            </a>
+          </li>
+          <li class="nav__item">
+            <a href="#" class="nav__link">
+              About Me
+            </a>
+          </li>
+          <li class="nav__item">
+            <a href="#" class="nav__link">
+              My Work
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
