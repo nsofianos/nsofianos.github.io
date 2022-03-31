3ddd4076c60b17c2c0629ffb0e99f312c7cc8566
nsofianos <nsofianos@hotmail.com>
2022-03-31 16:50:18 -0700

    add styles for titles/subtitles. also add dummy text to about section

--- src/components/About.js
+++ src/components/About.js
@@ -3,10 +3,29 @@ import portrait from "../img/dev-jane-02.jpg";
 function About() {
   return (
     <section className="about-me" id="about">
-      <h2 class="section__title section__title--about">Who I am</h2>
-      <p class="section__subtitle section__subtitle--about"></p>
+      <h2 className="section__title section__title--about">Who I am</h2>
+      <p className="section__subtitle section__subtitle--about">
+        Full Stack Developer based out of Vancouver
+      </p>
       <div className="about-me__body">
-        <p></p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
       </div>
       <img src={portrait} alt="portrait"></img>
     </section>
