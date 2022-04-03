dd0abd1287b438f5b0a1ce8aeb68545dd3655e01
nsofianos <nsofianos@hotmail.com>
2022-04-03 15:42:26 -0700

    add profile photo

--- src/components/Sections/Intro.js
+++ src/components/Sections/Intro.js
@@ -1,7 +1,13 @@
 import "./Intro.scss";
 
 function Intro() {
-  return <section className="intro" id="intro"></section>;
+  return (
+    <section className="intro" id="intro">
+      <div className="img-container">
+        <img src="images/me-circle2.png" alt="my profile pic"></img>
+      </div>
+    </section>
+  );
 }
 
 export default Intro;
