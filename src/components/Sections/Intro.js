851d89758c36b81aa5ee2d99539e013ffbb088a8
nsofianos <nsofianos@hotmail.com>
2022-04-04 18:38:23 -0700

    fix formatting & scrollbar issues

--- src/components/Sections/Intro.js
+++ src/components/Sections/Intro.js
@@ -3,8 +3,18 @@ import "./Intro.scss";
 function Intro() {
   return (
     <section className="intro" id="intro">
-      <div className="img-container">
-        <img src="images/me-circle2.png" alt="my profile pic"></img>
+      <div className="intro__left">
+        <div className="intro__img-container">
+          <img
+            className="intro__img"
+            src="images/me-circle2.png"
+            alt="my profile pic"
+          ></img>
+        </div>
+      </div>
+      <div className="intro__right">
+        <h2>Hi, I'm</h2>
+        <h1>Nik Sofianos</h1>
       </div>
     </section>
   );
