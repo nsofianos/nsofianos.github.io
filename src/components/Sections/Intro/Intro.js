ad99543408d299e8ae2905b926ec8bf968dd0ae8
nsofianos <nsofianos@hotmail.com>
2022-07-24 13:09:11 -0700

    update intro page with new photo/fonts

--- src/components/Sections/Intro/Intro.js
+++ src/components/Sections/Intro/Intro.js
@@ -10,7 +10,13 @@ function Intro() {
       showCursor: true,
       backSpeed: 50,
       backDelay: 1000,
-      strings: ["Developer", "Designer", "Gamer"],
+      strings: [
+        "Developer",
+        "Frontend Enthusiast",
+        "Freelancer",
+        "Gym Addict",
+        "Man of the People",
+      ],
     });
   }, []);
 
@@ -18,15 +24,15 @@ function Intro() {
     <section className="intro" id="intro">
       <div className="left">
         <div className="img-container">
-          <img alt="me" src="images/assets/man.png"></img>
+          <img alt="me" src="images/sketch2.jpg"></img>
         </div>
       </div>
       <div className="right">
         <div className="wrapper">
-          <h2>Hi There, I'm</h2>
+          <h2>Hi, I'm</h2>
           <h1>Nik Sofianos</h1>
           <h3>
-            Freelance <span ref={textRef}></span>
+            Full time <span ref={textRef}></span>
           </h3>
         </div>
         <a href="#projects">
